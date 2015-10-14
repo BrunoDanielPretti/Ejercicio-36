e4f6183c1344b2aab17ed57e6679bd792c66fd83
BrunoDanielPretti <alynya@gmail.com>
2015-10-14 09:04:51 -0300

    Primer Commit

---
+++ Ejercicio 36/Container.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_36
+{
+    class Container
+    {
+        private int _capacidad;
+        private string _empresa;
+        private List<Producto> _listaProductos;
+
+        public Container(int capacidad, string Empresa)
+        {
+            this._listaProductos = new List<Producto>();
+            this._listaProductos.Capacity = capacidad;
+            this._capacidad = capacidad;
+            this._empresa = Empresa;
+
+        }
+        //-------------------------------------------------------------
+        public static void Mostrar(Container Contenedor)
+        {
+            Console.WriteLine("-__-__Container__-__-\nEmpresa: " + Contenedor._empresa + "\nCapacidad: " + Contenedor._capacidad);
+            foreach (Producto item in Contenedor._listaProductos)
+            {
+                item.mostrar();
+            }
+        }
+
+        public bool Agregar(Producto proUno)
+        {
+            if(this._listaProductos.Count <= this._capacidad)
+                if (this != proUno)
+                {
+                    this._listaProductos.Add(proUno);
+                    return false;
+                }
+            return true;
+        }
+        //-------------------------------------------------------------
+        public static bool operator ==(Container contenedor, Producto proUno)
+        {
+            foreach (Producto item in contenedor._listaProductos)
+            {
+                if (item == proUno)
+                    return true;
+            }
+            return false;            
+        }
+        public static bool operator !=(Container contenedor, Producto proUno)
+        {
+            return !(contenedor == proUno);
+        }
+
+        public static List<Producto> operator -(Container contenedor, eTipoComestible tipo)
+        {
+            List<Producto> Lista = new List<Producto>();
+
+            foreach (Producto item in contenedor._listaProductos)
+            {
+                if (item == tipo)
+                    Lista.Add(item);
+            }            
+            return Lista;
+        }
+
+
+           
+    }
+}
---
+++ Ejercicio 36/Producto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ejercicio_36
+{
+    class Producto
+    {
+        private int _codigoDeBarra;
+        private string _nombre;
+        private double _precio;
+        public eTipoComestible _tipo;
+
+
+        private Producto(int codigoDeBarra)
+        {
+            this._codigoDeBarra = codigoDeBarra;
+        }
+        public Producto(int codigoDeBarra, string nombre, eTipoComestible tipo)
+            : this(codigoDeBarra)
+        {
+            this._nombre = nombre;
+            this._tipo = tipo;
+        }
+        public Producto(int codigoDeBarra, string nombre, eTipoComestible tipo, double precio)
+            : this(codigoDeBarra, nombre, tipo)
+        {
+            this._precio = precio;
+        }
+        
+        
+        //---------------------------------------------------------
+        public void mostrar()
+        {
+            Console.WriteLine("Nombre: " + this._nombre + "\nTipo: " + this._tipo + "\nCodigo: " + this._codigoDeBarra + "\nPrecio: " + this._precio);
+        }
+
+        //---------------------------------------------------------
+        public static bool operator ==(Producto proUno, Producto proDos)
+        {
+            if (proUno._nombre == proDos._nombre)
+                if (proUno._codigoDeBarra == proDos._codigoDeBarra)
+                    if (proUno._tipo == proDos._tipo)
+                        if (proUno._precio == proDos._precio)
+                            return true;
+            return false;
+        }
+        public static bool operator !=(Producto proUno, Producto proDos)
+        {
+            return !(proUno == proDos);
+        }
+
+        public static bool operator ==(Producto proUno, eTipoComestible tipo)
+        {
+            if (proUno._tipo == tipo)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Producto proUno, eTipoComestible tipo)
+        {
+            return !(proUno == tipo);
+        }
+        
+    }
+}
